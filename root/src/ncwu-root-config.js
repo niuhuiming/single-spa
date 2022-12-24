07d1f0cb9096dfd6abaf7e37b0323ca84dc396f3
nhm <3059350051@qq.com>
2022-12-24 20:27:23 +0800

    docs 添加基座模板代码注释

--- root/src/ncwu-root-config.js
+++ root/src/ncwu-root-config.js
@@ -1,5 +1,11 @@
 import { registerApplication, start } from "single-spa";
 
+/**
+ * 注册微前端应用
+ * 1.name: 字符串类型，微前端应用名称："@组织名称/应用名称"
+ * 2.app: 函数类型，返回 Promise, 通过 Systemjs 引用打包好的微前端引用模块代码
+ * 3.activeWhen: 路由匹配时激活应用
+ */
 registerApplication({
   name: "@single-spa/welcome",
   app: () =>
@@ -16,5 +22,7 @@ registerApplication({
 // });
 
 start({
+  // 是否可以通过 history.pushState() 和 history.resplaceState() 更改触发 single-spa 路由
+  // true 不允许
   urlRerouteOnly: true,
 });

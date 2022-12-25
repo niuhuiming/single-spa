0ed696a3c2b9a20ba2519c072fd0879bb1e98804
nhm <3059350051@qq.com>
2022-12-25 17:03:28 +0800

    feat 添加模块联邦基础文件

---
+++ webpack-federation/root/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import User from './User';
+
+export default function App() {
+  return (
+    <div>
+      <h3>webpack 5</h3>
+      <User />
+    </div>
+  )
+}
---
+++ webpack-federation/root/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // entry入口，output出口，module模块，plugins插件，mode工作模式，devServer开发服务器
+  mode: 'development', // production, development, none
+  entry: './src/index.js',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  // devServer: {
+  //   contentBase: path.join(__dirname, 'dist'),
+  //   port: 3000,
+  //   open: true
+  // }
+}
